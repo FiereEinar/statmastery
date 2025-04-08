a8488794bf0d5cd367aa2092beb5bf2ebd855c48
Nick Mours <nickxylanmelloria@gmail.com>
2025-04-09 05:38:34 +0800

    removed duplicate apline imports

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,7 +19,6 @@
 		@endif
 		<livewire:styles />
 		<wireui:scripts />
-		<script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.14.8/dist/cdn.min.js"></script>
 		<livewire:scripts />
 	</head>
 	<body class="relative">
